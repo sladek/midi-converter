82c6a4dd4115bb3114f4ef2a11e0f4da4a5701d5
Miro <sladek.miro@gmail.com>
2020-09-29 22:09:13 +0200

    Revert "Small fix for debugging purposes."
    This reverts commit 41272d75b45c8aef0d05e3af5fc9eb3e461f3f9e.

---
+++ src/USB_Device_Host_MIDI.X/build/PIC24FJ64GB002_PIM/debug/src/leds.o.d
@@ -0,0 +1,3 @@
+build/PIC24FJ64GB002_PIM/debug/src/leds.o:  \
+src/leds.c  \
+inc/leds.h 
---
+++ src/USB_Device_Host_MIDI.X/build/PIC24FJ64GB002_PIM/debug/usb/src/usb_host.o.d
@@ -0,0 +1,16 @@
+build/PIC24FJ64GB002_PIM/debug/usb/src/usb_host.o:  \
+usb/src/usb_host.c  \
+ inc/usb_device_config.h  \
+usb/inc/usb.h  \
+usb/inc/usb_common.h  \
+ usb/inc/usb_ch9.h  \
+usb/inc/usb_device.h  \
+usb/inc/usb_hal.h  \
+ usb/inc/usb_hal_pic24f.h  \
+usb/inc/usb_host_local.h  \
+ usb/inc/usb_hal_local.h  \
+usb/inc/usb.h  \
+usb/inc/usb_host.h  \
+ inc/usb_host_config.h  \
+usb/inc/usb_hal_local.h  \
+usb/inc/usb_struct_queue.h 
